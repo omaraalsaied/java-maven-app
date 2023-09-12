13272e76b1f0e894c494f7bb4af6c03e244ee528
Omar <74821022+omaraalsaied@users.noreply.github.com>
2023-09-12 03:20:02 +0300

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -19,6 +19,7 @@ public class Application {
     {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
+         log.info("jenkins should get a webhook !");
     }
 
     public String getStatus() {
